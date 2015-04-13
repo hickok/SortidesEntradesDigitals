f69032d1342ec9ccc20b34a6091b3e364db2a49f
hickok <jmcon@hotmail.es>
2015-04-13 21:01:31 +0200

    Exercici_4

---
+++ Exercici_4/Exercici_4.ino
@@ -0,0 +1,13 @@
+void setup()
+{
+  pinMode(5, OUTPUT);     // definir el pin 5 com una sortida
+  pinMode(6, OUTPUT);     // definir el pin 6 com una sortida
+  pinMode(7, OUTPUT);     // definir el pin 7 com una sortida
+  pinMode(8, OUTPUT);     // definir el pin 8 com una sortida
+}
+
+
+void loop()
+{
+  digitalWrite(5, LOW);    
+  digitalWrite(6, LOW); 
